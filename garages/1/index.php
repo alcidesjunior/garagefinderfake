62a529482a841d68755348b0121b00b3bd0a499b
Alcides Junior <alcidesjunior.infor@gmail.com>
2023-09-17 20:42:41 -0300

    Update index.php

--- garages/1/index.php
+++ garages/1/index.php
@@ -1,1 +1,1 @@
-"result": { "id":1, "parkingSpaces":50, "busySpace":10, "price":15.0, "lat":"-3.7438212", "long":"-38.5381543" }
+{ "id":1, "parkingSpaces":50, "busySpace":10, "price":15.0, "lat":"-3.7438212", "long":"-38.5381543" }
